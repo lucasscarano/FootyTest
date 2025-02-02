7add862e72f5b909836407a75761a92cadb2c59b
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2025-02-02 18:54:19 -0300

    small change to models

--- footballnerds/models.py
+++ footballnerds/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Team(models.Model):
-    team_id = models.CharField(primary_key=True, max_length=10)
+    team_id = models.IntegerField(primary_key=True, max_length=10)
     team_name = models.CharField(max_length=120, null=False)
 
     def __str__(self):
         return self.team_name
 
 class Player(models.Model):
-    player_id = models.CharField(primary_key=True, max_length=10)
+    player_id = models.IntegerField(primary_key=True, max_length=10)
     player_name = models.CharField(max_length=120, null=False)
 
     def __str__(self):
@@ -18,4 +18,4 @@ class Player(models.Model):
 
 class PlayerTeams(models.Model):
     player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
