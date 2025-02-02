1f314bcee754598cc5331ca1a48897226010f4e7
Lucas Scarano <69877425+lucasscarano@users.noreply.github.com>
2025-02-02 16:40:21 -0300

    initial commit

--- FootyTest/urls.py
+++ FootyTest/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("footballnerds", include("footballnerds.urls")),
+    path("", include("footballnerds.urls")),
     path('admin/', admin.site.urls),
 ]
--- footballnerds/models.py
+++ footballnerds/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    team_id = models.CharField(primary_key=True, max_length=10)
+    team_name = models.CharField(max_length=120, null=False)
+
+    def __str__(self):
+        return self.team_name
+
+class Player(models.Model):
+    player_id = models.CharField(primary_key=True, max_length=10)
+    player_name = models.CharField(max_length=120, null=False)
+    teams = models.ManyToManyField(Team)
+
+    def __str__(self):
+        return self.player_name
